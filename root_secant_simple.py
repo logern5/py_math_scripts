e43c3f9e87ed5bd1a847a761846669068982c6e0
logern5 <logern5@gmail.com>
2018-12-24 16:52:18 -0600

    Create root_secant_simple.py

---
+++ root_secant_simple.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+# Equation solver, using secant method, single root
+
+def solve(expr):
+	f = lambda x: eval(expr) # Input function
+	n = 1 # Initial guess
+	h = 0.1 # Tolerance
+	digits = 1 # Decimal digits to round to
+	# Make sure not at max or min
+	while f(n+h)-f(n) == 0:
+		n += h
+	# Find the root
+	while abs(f(n)) > h:
+		# Repeat quasi-Newtons method / Secant method
+		n = n - ( f(n) * h/(f(n+h)-f(n)) )
+	return round(n, digits)
+
+def test():
+	tests=["x**2-25", "x**3-x-2"]
+	
+	for test in tests:
+		print("Equation: {}".format(test))
+		print("Result: {}\n".format(solve(test)))
+
+test()
