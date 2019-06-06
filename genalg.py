3bc014510d5668274739f47cfba05d21179d8590
logern5 <logern5@gmail.com>
2019-06-06 12:10:37 -0500

    TODO: Add better selection and generation code

--- genalg.py
+++ genalg.py
@@ -1,9 +1,9 @@
 import random as rand
 
 cr_count = 3
-gene_count = 4
-mut_chance = 0.1
-generations = 10
+gene_count = 6
+mut_chance = 0.2
+generations = 40
 
 def ga_start():
 	pool = [None] * cr_count
@@ -22,6 +22,17 @@ def crossover(cra, crb):
 	crd = cra[p:] + crb[:p]
 	return [crc,crd]
 
+def mutate(pool):
+	idx = rand.randint(0,cr_count-1)
+	elem_idx = rand.randint(0,gene_count-1)
+	if(rand.random() < mut_chance):
+		print("Mutation!")
+		if(rand.randint(-1,1) > 0):
+			(pool[idx])[elem_idx] += 1
+		else:
+			(pool[idx])[elem_idx] -= 1
+	return pool
+
 def gen_alg():
 	pool = ga_start()
 	print("Pool={}".format(pool))
@@ -34,6 +45,7 @@ def gen_alg():
 		print("Sorted pool={}".format(pool))
 		pool_a = crossover(pool[0],pool[1])
 		pool = pool_a + pool[:(cr_count-2)]
+		pool = mutate(pool)
 		print("Pool={}".format(pool))
 
 gen_alg()
