a1a398ee116d791a2a00bff58548495746c5fe40
logern5 <logern5@gmail.com>
2019-06-05 21:51:13 -0500

    Create linreg.py

---
+++ linreg.py
@@ -0,0 +1,13 @@
+def reg(data):
+	# http://www.stat.ucla.edu/~rosario/classes/091/112-1b/regression.pdf
+	x_mean = 0
+	y_mean = 0
+	s_x = 0
+	s_y = 0
+	r = 0
+	n = 0
+	# y = mx+b, or y = bx+a, etc
+	b = 0
+	m = 0
+dat = []
+reg(dat)
