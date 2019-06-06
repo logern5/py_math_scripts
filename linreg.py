b8399c1a0da475ec9d690c7a8ba9834223d2c8c6
logern5 <logern5@gmail.com>
2019-06-05 22:43:10 -0500

    Update linreg.py

--- linreg.py
+++ linreg.py
@@ -1,13 +1,27 @@
+import math
 def reg(data):
 	# http://www.stat.ucla.edu/~rosario/classes/091/112-1b/regression.pdf
-	x_mean = 0
-	y_mean = 0
-	s_x = 0
-	s_y = 0
-	r = 0
-	n = 0
+	# get x and y components from data
+	x, y = zip(*data)
+	n = len(data)
+	x_mean = sum(i for i,j in data)/n
+	print(x_mean)
+	y_mean = sum(j for i,j in data)/n
+	print(y_mean)
+	s_x = math.sqrt(
+		sum((i - x_mean)**2 for i,j in data)/(n-1)
+	)
+	print(s_x)
+	s_y = math.sqrt(
+		sum((j - y_mean)**2 for i,j in data)/(n-1)
+	)
+	print(s_y)
+	r = sum((i - x_mean) * (j - y_mean) for i,j in data)/((n-1) * s_x * s_y)
+	print(r)
 	# y = mx+b, or y = bx+a, etc
-	b = 0
-	m = 0
-dat = []
+	m = r * (s_y/s_x)
+	print(m)
+	b = y_mean - m*x_mean
+	print(b)
+dat = [(10,40), (20,40), (40,60), (45,80), (60,90), (65,110)]
 reg(dat)
