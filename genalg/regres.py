3d11ce0eb9f184a11d2e36f26078c37ca06d5cd1
logern5 <logern5@gmail.com>
2019-06-07 16:20:25 +0100

    Add more gen algorithms

--- genalg/regres.py
+++ genalg/regres.py
@@ -1,34 +1,45 @@
-# Genetic algorithm to find a solution for 4 numbers that add up to 3
+# Linear regression using a genetic algorithm
+# There are better and easier algorithms for linear regression, this is just an example
 
 import random as rand
+import math
 
-cr_count = 6
-gene_count = 4
-mut_chance = 0.2
-generations = 20
+cr_count = 10
+gene_count = 2
+mut_chance = 0.4
+generations = 200
 
-# Create initial gene pool
+# Data to fit equations to
+# Expected equation: y=2x+5
+data = [(0,5),(2,9),(3,11),(8,21)]
+
 def ga_start():
 	pool = [None] * cr_count
 	for i in range(cr_count):
 		pool[i] = [None] * gene_count
 		for j in range(gene_count):
-			(pool[i])[j] = rand.randint(0,5)
+			(pool[i])[j] = rand.randint(0,7)
 	return pool
 
 def fitness(cr):
-	# The most fit solution has a sum closest to 3
-	# Highest score is 10
-	return -abs(3-sum(cr))+10
+	# Determine how well the model fits using Root Mean Square Error
+	m = cr[0]
+	b = cr[1]
+	n = len(data)
+	sum = 0
+	for i in data:
+		o = i[1]
+		f = m * i[0] + b
+		sum += (f - o)**2
+	rmse = math.sqrt(sum/n)
+	return 10-rmse
 
-# Cross over chromosomes cra and crb into two new offspring
 def crossover(cra, crb):
 	p = int(gene_count/2)
 	crc = cra[:p] + crb[p:]
 	crd = cra[p:] + crb[:p]
 	return [crc,crd]
 
-# Mutate a random gene on a random chromosome given by probability mut_chance
 def mutate(pool):
 	idx = rand.randint(0,cr_count-1)
 	elem_idx = rand.randint(0,gene_count-1)
@@ -40,28 +51,24 @@ def mutate(pool):
 			(pool[idx])[elem_idx] -= 1
 	return pool
 
-# Genetic algorithm to find an optimal solution to the problem of finding numbers that sum to 3
+# Genetic algorithm to fit an equation to data. y=mx+b, gene 0=m, gene 1=b
 def gen_alg():
-	# Get initial gene pool
 	pool = ga_start()
 	print("Pool={}".format(pool))
-	# Run genetic algorithm for specified
 	for gen in range(generations):
 		print("\nGen={}".format(gen))
-		# Sort gene pool by fittest chromosome (given by fitness function)
 		pool.sort(key=fitness)
 		pool = pool[::-1]
 		print("Pool={}".format(pool))
 		print("Max fitness={}".format(fitness(pool[0])))
-		# Crossover the two fittest parents and create two new offspring
 		pool_a = crossover(pool[0],pool[1])
-		# Create a new gene pool with the new offspring and the fittest of the last generation
 		pool = pool_a + pool[:(cr_count-2)]
-		# Randomly mutate a random gene on a random chromosome
 		pool = mutate(pool)
 	pool.sort(key=fitness)
 	pool = pool[::-1]
 	print("***END***")
 	print("Highest fitness score:{}".format(fitness(pool[0])))
 	print("Highest fitness chromosome:{}".format(pool[0]))
+	m,b = pool[0]
+	print("Approximate equation for data: y={}x+{}".format(m,b))
 gen_alg()
